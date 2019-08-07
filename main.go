035f9fb26bab3b146dbdce38d6118e1259fe82c5
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-08-07 11:06:56 -0500

    go module with chi server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Domain searches API"))
+	})
+	fmt.Println("Running server at: http://localhost:4001")
+
+	http.ListenAndServe(":4001", r)
+}
